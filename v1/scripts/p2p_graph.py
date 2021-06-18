7e36f29164ef80a3b230339994fe09cab23a260f
Anjan Roy <anjanroy@yandex.com>
2021-06-18 22:53:47 +0530

    add weight to edges --- data transferred over link

--- v1/scripts/p2p_graph.py
+++ v1/scripts/p2p_graph.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-from os import name, walk
+from os import walk
 from os.path import join, abspath
 from re import compile as reg_compile
-from typing import List, Tuple
+from typing import Dict, List, Tuple
 from json import loads
 from pyvis.network import Network
 
@@ -19,17 +19,32 @@ def get_file_paths(root: str) -> List[Tuple[int, str]]:
     return paths
 
 
-def read_and_plot(self_id: int, file: str):
+def read_and_plot(self_id: int, traffic_log: str, message_log: str):
     nt = Network(height='500px', width='100%',
                  bgcolor='#232323', font_color='white',
                  heading=f'P2P Network as seen by Peer {self_id}')
     nt.barnes_hut()
     nt.add_node(self_id, label=f'Peer {self_id}')
 
+    def read_traffic() -> Dict[int, Tuple[int, int]]:
+        traffic = {}
+        with open(traffic_log, 'r') as fd:
+            while True:
+                ln = fd.readline()
+                if not ln:
+                    break
+                vals = [int(i.strip()) for i in ln.split(';')]
+                traffic[vals[0]] = (vals[1], vals[2])
+        return traffic
+
+    traffic = read_traffic()
+
     def process_hops(hops: List[int]):
         peer = hops[-1]
+        weight = traffic[peer]
         nt.add_node(peer, label=f'Peer {peer}')
-        nt.add_edge(self_id, peer)
+        nt.add_edge(self_id, peer, value=sum(weight),
+                    title=f'In: {weight[0]} bytes | Out: {weight[0]} bytes')
         if len(hops) == 1:
             return
 
@@ -41,7 +56,7 @@ def read_and_plot(self_id: int, file: str):
             nt.add_node(hops[i+1], label=f'Peer {hops[i+1]}')
             nt.add_edge(hops[i], hops[i+1])
 
-    with open(file, 'r') as fd:
+    with open(message_log, 'r') as fd:
         while True:
             ln = fd.readline()
             if not ln:
@@ -55,7 +70,8 @@ def read_and_plot(self_id: int, file: str):
 def main():
     print('Processing p2p chit-chat log ...')
     for (peer, path) in get_file_paths('../../'):
-        read_and_plot(peer, path)
+        read_and_plot(peer, abspath(
+            join('../..', f'{peer}.traffic.txt')), path)
     print('Generated p2p graphs !')
 
 
