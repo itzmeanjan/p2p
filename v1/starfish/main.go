1f7c26c500d7d4b832cd953f32ee7fd4348c1d52
Anjan Roy <anjanroy@yandex.com>
2021-06-19 08:09:00 +0530

    starfish shaped network topology added

---
+++ v1/starfish/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"container/ring"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	v1 "github.com/itzmeanjan/p2p/v1"
+)
+
+type Info struct {
+	Id   uint32
+	Addr string
+}
+
+func generatePeers(total int) *ring.Ring {
+	r := ring.New(total)
+	for i := 0; i < r.Len(); i++ {
+		r.Value = &Info{
+			Id:   uint32(i),
+			Addr: fmt.Sprintf("127.0.0.1:700%d", i),
+		}
+		r = r.Next()
+	}
+	return r
+}
+
+func getPeers(r *ring.Ring) (*ring.Ring, map[uint32]string) {
+	peers := make(map[uint32]string)
+	tmp := r.Next()
+	p1 := tmp.Next().Value.(*Info)
+	p2 := tmp.Next().Next().Value.(*Info)
+	peers[p1.Id] = p1.Addr
+	peers[p2.Id] = p2.Addr
+	return r, peers
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	total := 5
+	peers := make([]*v1.Peer, 0, total)
+	r := generatePeers(total)
+	var _peers map[uint32]string
+
+	for i := 0; i < total; i++ {
+		r, _peers = getPeers(r)
+		cur := r.Value.(*Info)
+		peer, err := v1.New(ctx, cur.Id, cur.Addr, _peers, true)
+		if err != nil {
+			log.Printf("Failed to start peer %d : %s\n", i, err.Error())
+			return
+		}
+		peers = append(peers, peer)
+		r = r.Next()
+	}
+
+	done := make(chan struct{}, total)
+	for _, peer := range peers {
+		go peer.Client(ctx, done)
+	}
+
+	expected := 0
+	for range done {
+		expected++
+		if expected >= total {
+			break
+		}
+	}
+
+	interruptChan := make(chan os.Signal, 1)
+	signal.Notify(interruptChan, syscall.SIGTERM, syscall.SIGINT)
+
+	<-interruptChan
+	cancel()
+	<-time.After(time.Second)
+	for _, peer := range peers {
+		if err := peer.ExportTrafficCost(); err != nil {
+			log.Printf("Error encountered when exporting traffic cost : %s\n", err.Error())
+		}
+	}
+	log.Println("Shutdown")
+}
