e6b952ba4a0df47786c7f3ac05e781e070fece90
Anjan Roy <anjanroy@yandex.com>
2021-06-26 10:15:53 +0530

    randomised N-many neighbour finding

--- v2/main.go
+++ v2/main.go
@@ -1,22 +1,54 @@
 package main
 
 import (
-	"container/ring"
 	"context"
+	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
 	"github.com/itzmeanjan/p2p/v2/peer"
+	"github.com/multiformats/go-multiaddr"
 )
+
+func connect(ctx context.Context, p *peer.Peer, self int, peers []*peer.Peer, count int) error {
+	if count > len(peers)-1 {
+		return fmt.Errorf("can't make %d neighbours with %d peers", count, len(peers))
+	}
+
+	log.Printf("[%d] Finding neighbours\n", p.Id)
+	neighbours := make(map[multiaddr.Multiaddr]struct{})
+	for len(neighbours) != count {
+		n := rand.Intn(len(peers))
+		if n == self {
+			continue
+		}
+		p := peers[n]
+		addrs, err := p.GetAddress()
+		if err != nil {
+			return err
+		}
+		neighbours[addrs[0]] = struct{}{}
+	}
+
+	for n := range neighbours {
+		if err := p.Connect(ctx, n); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
 
 func main() {
 	var peerCount int64 = 8
+	var neighbourCount int = 2
 	ctx, cancel := context.WithCancel(context.Background())
 
-	peers := ring.New(int(peerCount))
+	peers := make([]*peer.Peer, 0, peerCount)
 	var i int64
 	for ; i < peerCount; i++ {
 		p, err := peer.NewPeer(ctx, i+1, 7001+int(i))
@@ -24,13 +56,12 @@ func main() {
 			log.Printf("Error: %s\n", err.Error())
 			return
 		}
-		peers.Value = p
-		peers = peers.Next()
+		peers = append(peers, p)
 	}
 
 	peer.InitContext(ctx)
-	peers.Do(func(i interface{}) {
-		p := i.(*peer.Peer)
+	for i := 0; i < int(peerCount); i++ {
+		p := peers[i]
 		addrs, err := p.GetAddress()
 		if err != nil {
 			log.Printf("Error: %s\n", err.Error())
@@ -39,28 +70,22 @@ func main() {
 		for _, addr := range addrs {
 			log.Printf("%d => %s\n", p.Id, addr)
 		}
-	})
+	}
 
-	for i := 0; i < peers.Len(); i++ {
-		cur := peers.Value.(*peer.Peer)
-		nxt := peers.Next().Value.(*peer.Peer)
-		addrs, err := nxt.GetAddress()
-		if err != nil {
+	<-time.After(time.Second)
+	for i := 0; i < int(peerCount); i++ {
+		if err := connect(ctx, peers[i], i, peers, neighbourCount); err != nil {
 			log.Printf("Error: %s\n", err.Error())
 			return
 		}
-		if err := cur.Connect(ctx, addrs[0]); err != nil {
-			log.Printf("Error: %s\n", err.Error())
-			return
-		}
-		peers = peers.Next()
 	}
 
 	<-time.After(4 * time.Second)
-	peers.Do(func(i interface{}) {
-		p := i.(*peer.Peer)
+	for i := 0; i < int(peerCount); i++ {
+		p := peers[i]
 		p.Probe()
-	})
+		<-time.After(4 * time.Second)
+	}
 
 	interruptChan := make(chan os.Signal, 1)
 	signal.Notify(interruptChan, syscall.SIGTERM, syscall.SIGINT)
@@ -68,10 +93,10 @@ func main() {
 	<-interruptChan
 	cancel()
 	<-time.After(time.Second)
-	peers.Do(func(i interface{}) {
-		p := i.(*peer.Peer)
+	for i := 0; i < int(peerCount); i++ {
+		p := peers[i]
 		p.Destroy()
 		p.ExportNetwork()
-	})
+	}
 	log.Println("Graceful shutdown !")
 }
