6dafda58b7dc98b0c0794cfc9b2789a390fa139d
Anjan Roy <anjanroy@yandex.com>
2021-06-27 10:26:10 +0530

    accumulative cost visualisation

--- v2/scripts/traffic.py
+++ v2/scripts/traffic.py
@@ -3,14 +3,14 @@
 from os import walk
 from os.path import join, abspath
 from re import compile as reg_compile
-from typing import List
+from typing import List, Tuple
 from pandas.core.frame import DataFrame
 from matplotlib import pyplot as plt
 import seaborn as sns
 
 
 def get_file_paths(root: str) -> List[str]:
-    reg = reg_compile(r'^((\d{1,})\.traffic\.csv)$')
+    reg = reg_compile(r'^((\d{1,}_\d{1,})\.cost\.csv)$')
     paths = []
     for (d, _, files) in walk(root):
         for file in files:
@@ -21,13 +21,20 @@ def get_file_paths(root: str) -> List[str]:
 
 
 def read(file: str, df: DataFrame) -> DataFrame:
-    def is_added(self_id: int, peer_id: int) -> bool:
+    def is_added(self_id: int, peer_id: int) -> Tuple[bool, int]:
         yes = False
-        for (frm, to, _, _, _) in df.values:
+        idx = 0
+        for (_idx, (frm, to, _, _, _)) in enumerate(df.values):
             if (frm == self_id and to == peer_id) or (frm == peer_id and to == self_id):
                 yes = True
+                idx = _idx
                 break
-        return yes
+        return yes, idx
+
+    def update_df(idx: int, add_cost: int, del_cost: int, probe_cost: int):
+        df.at[idx, 'add_cost'] += add_cost
+        df.at[idx, 'del_cost'] += del_cost
+        df.at[idx, 'probe_cost'] += probe_cost
 
     buf = []
     with open(file, 'r') as fd:
@@ -37,7 +44,8 @@ def read(file: str, df: DataFrame) -> DataFrame:
                 break
             [self_id, peer_id, add_cost, del_cost, probe_cost] = [
                 int(i.strip()) for i in ln.split(";")]
-            if (add_cost == 0 and del_cost == 0 and probe_cost == 0) or is_added(self_id, peer_id):
+            yes, idx = is_added(self_id, peer_id)
+            if yes:
                 continue
             buf.append({"from": self_id,
                         "to": peer_id,
@@ -52,8 +60,8 @@ def read_all(root: str) -> DataFrame:
     df = DataFrame()
     for p in get_file_paths(root):
         df = read(p, df)
+        df = df.reset_index(drop=True)
 
-    df = df.reset_index(drop=True)
     z = zip(df['from'].values, df['to'].values, df['add_cost'].values,
             df['del_cost'].values, df['probe_cost'].values)
     return DataFrame([{"edge": f'{i[0]} <-> {i[1]}', "add_cost": i[2], "del_cost": i[3], "probe_cost": i[4]} for i in z])
